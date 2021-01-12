3689b47cf44cb2c825a4e88ed075a2425f8daa08
Jeyluu <73641462+Jeyluu@users.noreply.github.com>
2021-01-12 18:21:49 +0100

    Methode GET OK

--- API GOLF projet 1 node/app.js
+++ API GOLF projet 1 node/app.js
@@ -2,13 +2,16 @@ const express = require('express');
 const bodyParser = require("body-parser");
 const mongoose = require('mongoose') ;
 const exphbs = require('express-handlebars');
+const Handlebars = require("handlebars");
+const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access');
+
 
 //express
 const app = express();
 
 //express-handlebars
 
-app.engine('hbs', exphbs({defaultLayout: "main", extname: "hbs"}));
+app.engine('hbs', exphbs({defaultLayout: "main", extname: "hbs", handlebars: allowInsecurePrototypeAccess(Handlebars)}));
 app.set('view engine', 'hbs');
 
 // BodyParser
@@ -19,21 +22,46 @@ app.use(bodyParser.urlencoded({
 
 //Connection avec Mongo DB
 
-mongoose.connect("mongodb://localhost:27017/GOLF-API", {
+mongoose.connect("mongodb://localhost:27017/golf", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
     useCreateIndex: true
 })
 
+//Schema pour la collection
+
+const trouSchema = {
+    trou: Number,
+    par: Number,
+    handicap: Number,
+    departrouge: Number,
+    departbleu: Number,
+    departjaune: Number,
+    departblanc: Number
+};
+
+const Trou = mongoose.model("ladomangeres", trouSchema );
 
 //Route
 //Pour affichage de la page Index dans le localhost
 app.route("/")
 //GET
 .get((req,res) => {
-    res.render("index")
+    Trou.find(function(err, information) {
+        if(!err) {
+            res.render("index", {
+                trou : information
+            })
+        } else {
+            res.send(err)
+        }
+
+        
+    })
 })
+
+
 //POST
 .post()
 //PUT
@@ -41,6 +69,6 @@ app.route("/")
 
 
 //Ecoute Serveur
-app.listen(1992, () => {
-    console.log("Vous êtes bien connecté au port 1992 de l'ordinateur.");
+app.listen(2000, () => {
+    console.log("Vous êtes bien connecté au port 2000 de l'ordinateur.");
 })
